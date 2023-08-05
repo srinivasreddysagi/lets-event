8fa31209d05e367161689ec6ecf68365c5e17b83
Nivas Reddy <srinivasreddysagi@gmail.com>
2023-08-05 15:21:24 +0530

    Initial setup for client Added basic setup for client application Setup project with next js react with sass configuration Uses TypeScript for development

---
+++ client/src/pages/App.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const App: FC = () => {
+  return <>Hello Nivas!</>
+};
+
+export default App;
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from "react";
+import App from "./App";
+
+const Home: FC = () => {
+  return <App />;
+};
+
+export default Home;
