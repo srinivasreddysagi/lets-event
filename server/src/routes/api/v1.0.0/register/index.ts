f1a95da660ff67ae7dece86aa9b51e2a3c4c493a
Nivas Reddy <srinivasreddysagi@gmail.com>
2023-08-13 16:30:26 +0530

    Server-side validation in registration flow

--- server/src/routes/api/v1.0.0/register/index.ts
+++ server/src/routes/api/v1.0.0/register/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import Register from "../../../../models/Register";
+import * as messages from "../../../../assets/content/AlertMessages.json";
 
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.get("/", async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const registrationData = new Register({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -22,8 +23,22 @@ router.post('/', (req, res) => {
     });
 
     try {
-        const dbResponse = registrationData.save();
-        res.json(dbResponse);
+        const registeredUser = await Register.find({
+            email: req.body.email,
+        });
+        if (registeredUser.length > 0) {
+            res.json({
+                type: messages.alertVariants.info,
+                message: messages.registration.exist,
+            });
+        } else {
+            const dbResponse = registrationData.save();
+            res.json({
+                dbOut: dbResponse,
+                type: messages.alertVariants.success,
+                message: messages.registration.done,
+            });
+        }
     } catch(error) {
         res.json({ message: error });
     }
