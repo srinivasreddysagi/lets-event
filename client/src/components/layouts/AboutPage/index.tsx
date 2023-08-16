eaef0f6fd31ef704841668f31f16681fd8028353
Nivas Reddy <srinivasreddysagi@gmail.com>
2023-08-16 23:50:08 +0530

    Site content in Landing and About pages

--- client/src/components/layouts/AboutPage/index.tsx
+++ client/src/components/layouts/AboutPage/index.tsx
@@ -1,10 +1,21 @@
 import React, { FC } from "react";
 import NavBar from "../../NavBar";
+import Jambotron from "../../common/Jambotron/Jambotron";
+import content from "../../../assets/content/SiteContent.json";
+import appData from "../../../assets/content/AppDetails.json";
 
 export const AboutPage: FC = () => {
     return (
         <>
             <NavBar />
+            <main className="container py-4">
+                <Jambotron
+                    pickupLine={content.aboutPage.pickupLine}
+                    moreDetails={content.aboutPage.details}
+                    label={content.aboutPage.createAccount}
+                    link={appData.routes.data[2].path}
+                />
+            </main>
         </>
     );
 };
--- client/src/components/layouts/LandingPage/index.tsx
+++ client/src/components/layouts/LandingPage/index.tsx
@@ -1,10 +1,21 @@
 import React, { FC } from "react";
+import Jambotron from "../../common/Jambotron/Jambotron";
 import NavBar from "../../NavBar";
+import content from '../../../assets/content/SiteContent.json';
+import appData from "../../../assets/content/AppDetails.json";
 
 export const LandingPage: FC = () => {
     return (
         <>
             <NavBar />
+            <main className="container py-4">
+                <Jambotron
+                    pickupLine={content.landingPage.pickupLine}
+                    moreDetails={content.landingPage.details}
+                    label={content.landingPage.createAccount}
+                    link={appData.routes.data[2].path}
+                />
+            </main>
         </>
     );
 };
