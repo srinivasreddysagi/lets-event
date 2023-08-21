a8a2046e8b93c3f3cef3fc038bc712e6d72e0c0f
Nivas Reddy <srinivasreddysagi@gmail.com>
2023-08-21 10:58:40 +0530

    Made button in Jumbotron optional

--- client/src/components/common/Jambotron/index.tsx
+++ client/src/components/common/Jambotron/index.tsx
@@ -6,16 +6,22 @@ export const Jambotron = ({ pickupLine, moreDetails, label, link }) => {
 
     const clickHandler = () => {
         router.push(link);
-    }
+    };
 
     return (
         <div className="p-5 mb-4 bg-light rounded-3">
             <div className="container-fluid py-5">
                 <h1 className="display-5 fw-bold">{pickupLine}</h1>
                 <p className="col-md-10 fs-4">{moreDetails}</p>
-                {label && <button className="btn btn-primary btn-lg" type="button" onClick={clickHandler}>
-                    {label}
-                </button>}
+                {label && (
+                    <button
+                        className="btn btn-primary btn-lg"
+                        type="button"
+                        onClick={clickHandler}
+                    >
+                        {label}
+                    </button>
+                )}
             </div>
         </div>
     );
