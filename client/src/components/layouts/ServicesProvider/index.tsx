d80b137e3ae90d163a0a01fae778a00968a5cc5f
Nivas Reddy <srinivasreddysagi@gmail.com>
2023-08-27 18:02:48 +0530

    ServicesProvider - layout component

--- client/src/components/layouts/ServicesProvider/index.tsx
+++ client/src/components/layouts/ServicesProvider/index.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import AddItemTile from "../../AddItemTile";
+import AddServiceForm from "../../AddServiceForm";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { setIsLoading } from "../../../store/slices/acrossAppSlice";
+import { setProviderServices } from "../../../store/slices/eventServices";
+import { getRequest } from "../../../services/ApiCalls";
+import endpoints from '../../../assets/content/Endpoints.json';
 
-export const ServicesProvider = () => {
-    return <div>ServicesProvider</div>;
+export const ServicesProvider: FC = () => {
+    const dispatch = useAppDispatch();
+    const userData = useAppSelector((state) => state.acrossApp.userData);
+
+    const fetchServicesOfProvider = async (email = "") => {
+        const payload = {
+            email
+        }
+        const allServices = await getRequest(
+            endpoints.root +
+                endpoints.endpoints.rootVersion +
+                endpoints.endpoints.allServices,
+            payload
+        );
+        dispatch(setProviderServices(allServices));
+    }
+
+    useEffect(() => {
+        dispatch(setIsLoading(true));
+        fetchServicesOfProvider(userData.email);
+        dispatch(setIsLoading(false));
+    }, [])
+
+    const [open, setOpen] = useState(false);
+    const providerServices = useAppSelector(
+        (state) => state.eventServices.providerServices
+    );
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Container className="services-provider empty">
+                <Row className="p-4">
+                    {providerServices.map((service) => (
+                        <Col lg={4} className=" p-4" key={service.id}>
+                            <AddItemTile clickHandler={() => setOpen(true)} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+            <AddServiceForm
+                open={open}
+                handleClose={handleClose}
+                userData={userData}
+            />
+        </>
+    );
 };
 
 export default ServicesProvider;
