85404586e1106dde13a4cdc93329701960b3299d
Nivas Reddy <srinivasreddysagi@gmail.com>
2023-08-27 18:05:19 +0530

    EventServices - layout component

--- client/src/components/layouts/EventServices/index.tsx
+++ client/src/components/layouts/EventServices/index.tsx
@@ -4,10 +4,14 @@ import React from "react";
 export const EventServices = () => {
     return (
         <main className="event-services">
-            <div className="container">
-                <p>Choose which ever service you want</p>
-                <Link href={"/event-services/seeker"}>Seeker</Link>
-                <Link href={"/event-services/provider"}>Provider</Link>
+            <div className="container d-flex flex-column align-items-center">
+                <h3 className="choose-prompt">
+                    What are you?
+                </h3>
+                <div className="links-wrapper d-flex justify-items-center gap-4">
+                    <Link href={"/event-services/seeker"}>Seeker</Link>
+                    <Link href={"/event-services/provider"}>Provider</Link>
+                </div>
             </div>
         </main>
     );
